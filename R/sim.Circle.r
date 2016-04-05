8e2d1744d1ac8b72c92069cfc15a690267ac4d21
merliseclyde <clyde@stat.duke.edu>
2016-04-04 22:20:06 -0400

    fixed nc=

--- R/sim.Circle.r
+++ R/sim.Circle.r
@@ -3,7 +3,7 @@ sim.Circle <- function(n, dim=5) {
   if (dim < 2) {
     stop ("number of variables must be >= 2.");
   }
-  x <- matrix(runif(n*dim, min=-1, max=1), nr=n);
+  x <- matrix(runif(n*dim, min=-1, max=1), nrow=n);
   r2 <- x[, 1]^2 + x[, 2]^2;
   y <- rep(0, n);
   y[r2 <= 2/pi] <- 1;
