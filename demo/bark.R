66269b6fab40c027b90d2ce28e33dcb34b61f1b1
merliseclyde <clyde@stat.duke.edu>
2020-10-12 12:31:03 -0400

    updated vignettes and demo

--- demo/bark.R
+++ demo/bark.R
@@ -1,4 +1,4 @@
-##Simulate regression example
+## Simulate regression example
 # Friedman 2 data set, 200 noisy training, 1000 noise free testing
 # Out of sample MSE in SVM (default RBF): 6500 (sd. 1600)
 # Out of sample MSE in BART (default):    5300 (sd. 1000)
@@ -8,7 +8,7 @@ fit.bark.d <- bark(traindata$x, traindata$y, testdata$x, classification=FALSE, t
 boxplot(as.data.frame(fit.bark.d$theta.lambda))
 mean((fit.bark.d$yhat.test.mean-testdata$y)^2)
 
-##Simulate classification example
+## Simulate classification example
 # Circle 5 with 2 signals and three noisy dimensions
 # Out of sample erorr rate in SVM (default RBF): 0.110 (sd. 0.02)
 # Out of sample error rate in BART (default):    0.065 (sd. 0.02)
