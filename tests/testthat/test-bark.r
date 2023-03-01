19be4a5eda923f721becb2b982628e3f9ddc34fe
Merlise Clyde <clyde@stat.duke.edu>
2023-02-28 23:24:37 -0500

    added additional tests to cover 'type' in bark

--- tests/testthat/test-bark.r
+++ tests/testthat/test-bark.r
@@ -18,6 +18,35 @@ test_that("new bark", {
                 mean((fit.bark$yhat.test.mean-testdata$y)^2))
    
    set.seed(42)
+   fit.bark.depc <- bark_mat(traindata$x, traindata$y, testdata$x,
+                             nburn=10, nkeep=100, keepevery=10,
+                             classification=FALSE, type="e", printevery=10^10)
+   set.seed(42)
+   fit.bark  <- bark(y ~ ., data=data.frame(traindata), x.test=testdata$x,
+                     nburn=10, nkeep=100, keepevery=10,
+                     classification=FALSE, type="e", printevery=10^10)
+   
+   
+   
+   expect_equal(mean((fit.bark.depc$yhat.test.mean-testdata$y)^2),
+                mean((fit.bark$yhat.test.mean-testdata$y)^2))
+   
+   
+   set.seed(42)
+   fit.bark.depc <- bark_mat(traindata$x, traindata$y, testdata$x,
+                             nburn=10, nkeep=100, keepevery=10,
+                             classification=FALSE, type="sd", printevery=10^10)
+   set.seed(42)
+   fit.bark  <- bark(y ~ ., data=data.frame(traindata), x.test=testdata$x,
+                     nburn=10, nkeep=100, keepevery=10,
+                     classification=FALSE, type="sd", printevery=10^10)
+   
+   
+   
+   expect_equal(mean((fit.bark.depc$yhat.test.mean-testdata$y)^2),
+                mean((fit.bark$yhat.test.mean-testdata$y)^2))
+   
+   set.seed(42)
    n = 500
    circle2 = data.frame(sim_circle(n, dim = 2))
    train = sample(1:n, size = floor(n/2), rep=FALSE)
