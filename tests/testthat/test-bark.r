75e4dfd88d214c146c1aaf124473e44afecd5415
Merlise Clyde <clyde@stat.duke.edu>
2023-02-28 21:31:38 -0500

    added unit test for classification bark

--- tests/testthat/test-bark.r
+++ tests/testthat/test-bark.r
@@ -1,4 +1,6 @@
 test_that("new bark", {
+  
+  #regression
   traindata <- sim.Friedman2(200, sd=125)
   testdata <- sim.Friedman2(1000, sd=0)
   set.seed(42)
@@ -14,5 +16,29 @@ test_that("new bark", {
   
    expect_equal(mean((fit.bark.depc$yhat.test.mean-testdata$y)^2),
                 mean((fit.bark$yhat.test.mean-testdata$y)^2))
+   
+   set.seed(42)
+   n = 500
+   circle2 = data.frame(sim_circle(n, dim = 2))
+   train = sample(1:n, size = floor(n/2), rep=FALSE)
+   set.seed(42)
+   circle2.bark = bark(y ~ ., data=circle2, subset=train,
+                       x.test = as.matrix(circle2[-train, 1:2]),
+                       classification = TRUE,
+                       type="se",
+                       nburn = 10,
+                       nkeep = 100,
+                       printevery = 10^10)
+   set.seed(42)
+   circle2.bark.depr = bark_mat(y.train=circle2[train,"y"], 
+                                x.train=as.matrix(circle2[train, 1:2]),
+                                x.test = as.matrix(circle2[-train, 1:2]),
+                                classification = TRUE,
+                                type="se",
+                                nburn = 10,
+                                nkeep = 100,
+                                printevery = 10^10)
+   expect_equal(mean((circle2.bark$yhat.test.mean > 0) != circle2[-train, "y"]),
+                mean((circle2.bark.depr$yhat.test.mean > 0) != circle2[-train, "y"]))
                 
 })
